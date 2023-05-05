9e684151e499961bc5a14625ff298db5a2e36982
FlutterDevASMR <flutterdevasmr@gmail.com>
2023-05-05 17:13:01 +0430

    login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,18 +1,266 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:plant_store_app/screens/home_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool remeberMe = false;
+  bool passwordVisible = false;
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return WillPopScope(
       onWillPop: () async {
         return false;
       },
       child: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Text('Login Screen'),
+          child: Column(
+            children: [
+              Text(
+                'Plant App',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.green,
+                ),
+              ),
+              Text(
+                'Login to Your Account',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 27,
+                ),
+              ),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width / 15),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                          ),
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          filled: true,
+                        ),
+                        validator: (value) => EmailValidator.validate(value!)
+                            ? null
+                            : 'Please enter a valid email',
+                      ),
+                    ),
+                    SizedBox(height: height * 0.03),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width / 15),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.lock,
+                          ),
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          filled: true,
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                passwordVisible = !passwordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        obscureText: !passwordVisible,
+                        validator: (value) => value!.length >= 8
+                            ? null
+                            : 'Password must be more than 8 characters',
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: remeberMe,
+                          onChanged: (value) {
+                            setState(() {
+                              remeberMe = value!;
+                            });
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            side: BorderSide(
+                              color: Colors.green,
+                            ),
+                          ),
+                        ),
+                        Text('Remember me'),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width / 15),
+                      child: SizedBox(
+                        width: width,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              showDialog(
+                                  barrierDismissible: false,
+                                  context: context,
+                                  builder: (context) {
+                                    return WillPopScope(
+                                      onWillPop: () async {
+                                        return false;
+                                      },
+                                      child: AlertDialog(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        content: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Container(
+                                              child: Icon(
+                                                Icons.person,
+                                                size: 40,
+                                                color: Colors.white,
+                                              ),
+                                              decoration: BoxDecoration(
+                                                color: Colors.green,
+                                                shape: BoxShape.circle,
+                                              ),
+                                              padding: EdgeInsets.all(30),
+                                            ),
+                                            SizedBox(height: height * 0.04),
+                                            Text(
+                                              'Welcome Back!',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            SizedBox(height: height * 0.04),
+                                            LoadingAnimationWidget.hexagonDots(
+                                                color: Colors.green, size: 50),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  });
+                            }
+                            Future.delayed(Duration(seconds: 3), () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => HomeScreen(),
+                                  ));
+                            });
+                          },
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: height * 0.02),
+                    Text(
+                      'Forgot the password?',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'or continue with',
+              ),
+              Row(
+                children: [
+                  Container(
+                    child: FaIcon(
+                      FontAwesomeIcons.facebook,
+                    ),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  Container(
+                    child: FaIcon(
+                      FontAwesomeIcons.google,
+                    ),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  Container(
+                    child: FaIcon(
+                      FontAwesomeIcons.apple,
+                    ),
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           ),
         ),
       ),
